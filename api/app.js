41846ba6f16c203911d7a71a17e002e17d406003
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-22 13:02:45 +0200

    add hashed password to register controller

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import postRoute from "./routes/post.route.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,11 @@
-export const register = (req, res) => {
-  // db operations
-  console.log("register endpoint");
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
 };
 export const login = (req, res) => {
   // db operations

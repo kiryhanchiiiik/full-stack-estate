7cafc30cab823a7269d43600f53ca3b690fa5a1b
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-26 13:48:33 +0200

    add AuthContext to client side

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

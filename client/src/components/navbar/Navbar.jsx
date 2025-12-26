c4de7f59549dcf4d18cb1931661b5363ebca3342
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-26 14:00:11 +0200

    set user data dynamic

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
-import "./navbar.scss";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./navbar.scss";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const user = true;
   return (
@@ -19,13 +22,10 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        {user ? (
+        {currentUser ? (
           <div className="user">
-            <img
-              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt=""
-            />
-            <span>John Doe</span>
+            <img src={currentUser.avatar || "/noavatar.jpeg"} alt="" />
+            <span>{currentUser.username}</span>
             <Link to="/profile" className="profile">
               <div className="notification">3</div>
               <span>Profile</span>

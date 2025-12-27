fe0db66594cf4d860a195eeee6d448c8c9ac288b
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-27 10:15:07 +0200

    structured user part

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,30 @@
+export const getUsers = async (req, res) => {
+  console.log("it works");
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get users!" });
+  }
+};
+export const getUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get user!" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to update users!" });
+  }
+};
+export const deleteUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,20 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "./../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
-router.get("/:id", getUser);
+router.get("/:id", verifyToken, getUser);
 
-router.put("/:id", updateUser);
+router.put("/:id", verifyToken, updateUser);
 
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;

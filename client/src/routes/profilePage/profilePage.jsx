8ae3d2ad61f3f591fa73b44540c95ad6ccb9009f
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-27 09:50:58 +0200

    update profilePage with real data

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,15 +1,20 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./../../context/AuthContext";
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import apiRequest from "./../../lib/apiRequest";
 import "./profilePage.scss";
 
 function ProfilePage() {
+  const { updateUser, currentUser } = useContext(AuthContext);
+
   const navigate = useNavigate();
   const handleLogout = async () => {
     try {
       const res = await apiRequest.post("/auth/logout");
       localStorage.removeItem("user");
+      updateUser(null);
       navigate("/");
     } catch (err) {
       console.log(err);
@@ -27,16 +32,13 @@ function ProfilePage() {
           <div className="info">
             <span>
               Avatar:
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
+              <img src={currentUser.avatar || "noavatar.jpeg"} alt="" />
             </span>
             <span>
-              Username: <b>John Doe</b>
+              Username: <b>{currentUser.username}</b>
             </span>
             <span>
-              E-mail: <b>john@gmail.com</b>
+              E-mail: <b>{currentUser.email}</b>
             </span>
             <button onClick={handleLogout}>Logout</button>
           </div>

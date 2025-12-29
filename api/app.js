d71392f6deeba79033a2028b4fb1cb9f49511491
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-29 12:31:01 +0200

    add to app.js user requests

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 import "dotenv/config";
 
 const app = express();
@@ -12,9 +13,10 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");

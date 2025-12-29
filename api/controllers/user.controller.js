e4489b16c832c1634cd674b809c24bf9ec8885bc
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-29 12:46:41 +0200

    add updateUser to user.controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 
 export const getUsers = async (req, res) => {
@@ -23,7 +24,29 @@ export const getUser = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+
+  const { password, avatar, ...inputs } = req.body;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: "Not Authorized!" });
+  }
+  let updatedPassword = null;
   try {
+    if (password) {
+      updatedPassword = await bcrypt.hash(password, 10);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        ...inputs,
+        ...(updatedPassword && { password: updatedPassword }),
+        ...(avatar && { avatar }),
+      },
+    });
+    res.status(200).json(updatedUser);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update users!" });

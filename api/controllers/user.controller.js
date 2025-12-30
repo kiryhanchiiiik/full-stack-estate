789f1e4140e14eb852b4874e0b6ba54aeb0120f9
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-30 15:53:45 +0200

    delete password from output

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,12 +46,16 @@ export const updateUser = async (req, res) => {
         ...(avatar && { avatar }),
       },
     });
-    res.status(200).json(updatedUser);
+    const { password: userPassword, ...rest } = updatedUser;
+
+    res.status(200).json(rest);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update users!" });
   }
 };
+
+
 export const deleteUser = async (req, res) => {
   const id = req.params.id;
   const tokenUserId = req.userId;
